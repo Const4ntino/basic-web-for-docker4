4c59c2f94eaef3fe8a59fce1e45bcf494bd7ed31
Const4ntino <adrianalvaradoconstantino@gmail.com>
2026-08-18 13:04:30 -0500

    clean database.php

--- includes/database.php
+++ includes/database.php
@@ -8,16 +8,11 @@ class Database
     public static function getConnection(): PDO
     {
         if (self::$instance === null):
-            $host = getenv('DB_HOST');
-            $port = getenv('DB_PORT');
-            $dbName = getenv('DB_NAME');
-            $user = getenv('DB_USER');
-            $password = getenv('DB_PASSWORD');
-            // $host = getenv('DB_HOST') ?: "127.0.0.1";
-            // $port = getenv('DB_PORT') ?: "5432";
-            // $dbName = getenv('DB_NAME') ?: "veterinaria";
-            // $user = getenv('DB_USER') ?: "postgres";
-            // $password = getenv('DB_PASSWORD') ?: "postgres";
+            $host = $_ENV('DB_HOST');
+            $port = $_ENV('DB_PORT');
+            $dbName = $_ENV('DB_NAME');
+            $user = $_ENV('DB_USER');
+            $password = $_ENV('DB_PASSWORD');
             
             $dsn = "pgsql:host={$host};port={$port};dbname={$dbName};options='--client_encoding=UTF8'";
 
